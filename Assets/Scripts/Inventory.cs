67e87c470e02d291455d3cded10e3b48b6c6a7fc
Jonathan Fong <12115684+robofin117@users.noreply.github.com>
2017-12-16 16:05:15 -0800

    Inventory Changes
    Further improvement on inventory system.
    -Fleshed out inventory layout
    -Added prefabs for inventory slots and a placeholder item icon.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private RectTransform inventoryRectangle;
+    private float inventoryWidth, inventoryHeight;
+    public int slots;
+    public int rows;
+    public float slotPaddingLeft, slotPaddingTop;
+    public float slotSize;
+    public GameObject slotPrefab;
+    private List<GameObject> allSlots; 
+
+	// Use this for initialization
+	void Start () {
+        CreateLayout();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void CreateLayout()
+    {
+        allSlots = new List<GameObject>();
+        inventoryWidth = (slots / rows) * (slotSize + slotPaddingLeft) + slotPaddingLeft;
+        inventoryHeight = (rows) * (slotSize + slotPaddingTop) + slotPaddingTop;
+        inventoryRectangle = GetComponent<RectTransform>();
+        inventoryRectangle.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+        inventoryRectangle.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryHeight);
+
+        int columns = slots / rows;
+        for (int y = 0; y< rows; y++)
+        {
+            for(int x = 0; x<columns; x++)
+            {
+                GameObject newSlot = (GameObject)Instantiate(slotPrefab);
+                RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+                newSlot.name = "Slot";
+                newSlot.transform.SetParent(this.transform.parent);
+                slotRect.localPosition = inventoryRectangle.localPosition + new Vector3(slotPaddingLeft * (x + 1) + (slotSize * x), -slotPaddingTop * (y + 1) - (slotSize * y),0);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
+
+                allSlots.Add(newSlot);
+            }
+        }
+    }
+
+
+}
