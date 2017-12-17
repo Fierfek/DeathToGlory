a96241efd1bec895c39be50d8738784e1e280adb
Brennan Zuber <geekssw@yahoo.com>
2017-12-16 19:55:05 -0800

    Camera revamp

---
+++ Assets/MouseRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour {
+
+	public float rotationSpeedX, rotationSpeedY;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * rotationSpeedX, 0));
+		transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * rotationSpeedY, 0, 0));
+		transform.Rotate(0, 0, -transform.eulerAngles.z);
+	}
+}
--- Assets/Scripts/ControlScheme.cs
+++ Assets/Scripts/ControlScheme.cs
@@ -6,45 +6,65 @@
 
 public class ControlScheme : MonoBehaviour {
 
+	private Vector3 moveDirection;
 	public float moveSpeed = 1;
 	public float sprintSpeed = 2;
 	public float jumpSpeed = 1;
 	public float gravity = 1;
-	public float cameraPanSpeedX = 1;
-	public float cameraPanSpeedY = 1;
-
-	Vector3 moveDirection;
 
 	CharacterController cc;
+
+	public GameObject cameraAnchor;
+	private float radToDeg = 180 / Mathf.PI;
+	private Quaternion cameraRotation;
+	private Vector3 forward, right;
+
+	//inputs
+	private float currentRotation;
+	public float turnRate = .75f;
 
 	private void Start() {
 		cc = GetComponent<CharacterController>();
 	}
 
 	// Update is called once per frame
-	void Update () {
-		//This is close to out of combat controls.
-		if(cc.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
+	void Update() {
 
-			if(Input.GetButton("Sprint")) {
+		if (cc.isGrounded) {
+			//Find the foreward relative to the camera
+			forward = cameraAnchor.transform.forward.normalized;
+			forward.y = 0;
+			right = new Vector3(forward.z, 0, -forward.x);
+
+			//set the movement direction
+			moveDirection = Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward;
+
+			if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+				//save current camera rotation, rotate the character, then unrotate the camera.
+				cameraRotation = cameraAnchor.transform.rotation;
+				RotateTo(Mathf.Atan2(moveDirection.x, moveDirection.z) * radToDeg);
+				cameraAnchor.transform.rotation = cameraRotation;
+			}
+
+			if (Input.GetButton("Sprint")) {
 				moveDirection *= sprintSpeed;
 			} else {
 				moveDirection *= moveSpeed;
 			}
-			
 
 			if (Input.GetButton("Jump")) {
 				moveDirection.y = jumpSpeed;
 			}
 
 		}
+
+		//set gravity & move;
 		moveDirection.y -= gravity * Time.deltaTime;
 		cc.Move(moveDirection * Time.deltaTime);
+	}
 
-		transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X")  * cameraPanSpeedX, 0));
-		transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * cameraPanSpeedY, 0, 0));
-		transform.Rotate(0, 0, -transform.eulerAngles.z);
+	private void RotateTo(float angle) {
+		currentRotation = transform.eulerAngles.y;
+		transform.eulerAngles = new Vector3(0, Mathf.LerpAngle(currentRotation, angle, turnRate), 0);
 	}
 }
