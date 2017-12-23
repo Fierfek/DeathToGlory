683111716fc5098fa2ed0b55663f547e2a2e65fe
Jonathan Fong <robofin117@gmail.com>
2017-12-23 13:38:15 -0800

    Inventory Button
    Use i to open and close inventory.

---
+++ Assets/Scripts/inventoryOpen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventoryOpen : MonoBehaviour {
+
+	bool currentState;
+	public GameObject inventoryPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Inventory")) {
+			currentState = !currentState;
+			inventoryPanel.SetActive(currentState);
+		}
+	}
+}
