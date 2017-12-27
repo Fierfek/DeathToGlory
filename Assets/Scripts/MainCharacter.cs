acd1024107f415e816d6704db5d2502e208ad317
Fierfek <geekssw@yahoo.com>
2017-12-26 17:37:47 -0800

    Merge fix for combat update

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -2,34 +2,51 @@
 
 [RequireComponent(typeof(Health))]
 [RequireComponent(typeof(ControlScheme))]
-
+//hello
 public class MainCharacter : MonoBehaviour {
 
 	Health health;
-	public GameObject axeCollider;
-	public Attack attack;
+	public Inventory inventory;
+	public static bool updateStats;
+	public UniqueSlot[] equipped;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		health = GetComponent<Health>();
 
 		health.setHealth(100);
+
+
+		CharacterStats.moveSpeed = 5f;
+		CharacterStats.sprintSpeed = 10f;
+		CharacterStats.jumpSpeed = 5f;
+		CharacterStats.gravity = 9.8f;
+		CharacterStats.armor = 3;
+		CharacterStats.magicAtk = 3;
+		CharacterStats.magicDef = 3;
+		CharacterStats.attack = 3;
+		CharacterStats.spirit = 0;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		//Check for death
-		if(health.getHealth() <= 0) {
-			//Die
-			//ResetGame
-		}
-
-		if (Input.GetButton("Fire1") ) {
-			basicAttack();
+	void Update() {
+		if (updateStats) {
+			foreach (UniqueSlot slot in equipped) {
+				CharacterStats.armorMod += slot.CurrentItem.armorMod;
+				CharacterStats.movementMod += slot.CurrentItem.movementMod;
+				CharacterStats.magicAtkMod += slot.CurrentItem.magicAtkMod;
+				CharacterStats.magicDefMod += slot.CurrentItem.magicDefMod;
+				CharacterStats.attackMod += slot.CurrentItem.attackMod;
+				CharacterStats.spiritMod += slot.CurrentItem.spiritMod;
+			}
+			updateStats = false;
 		}
 	}
 
-	public void basicAttack() {
-		attack.DoAttack(.5f);
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Item") {
+			inventory.AddItem(other.GetComponent<Item>());
+		}
 	}
 }
