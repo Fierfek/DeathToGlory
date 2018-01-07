1fd9ea3bbba3986d1bc32ef6ac6e64643678510f
Fierfek <geekssw@yahoo.com>
2018-01-07 11:36:08 -0800

    Hook
    Forgot hook code commit

--- Assets/Hook.cs
+++ Assets/Hook.cs
@@ -3,39 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class Hook : MonoBehaviour {
 
-	private float speed = .6f, distance;
+	private float speed = 20f, distance;
 	private bool throwing, done;
 	private Vector3 start, end;
 	public GameObject player;
+
 	CharacterController cc;
+	Rigidbody rigidbody;
+	public GameObject chainSection;
+	GameObject[] chains;
+	int i = 0;
+	Vector3 offset;
+	
 
 	// Use this for initialization
 	void Start () {
+		chains = new GameObject[50];
 
+		cc = player.GetComponent<CharacterController>();
+		rigidbody = GetComponent<Rigidbody>();
+		gameObject.GetComponent<SphereCollider>().isTrigger = true;
+
+		rigidbody.useGravity = false;
+		rigidbody.isKinematic = true;
+
+		offset = new Vector3(0,  -0.05f, 0);
+
+		for (i = 0; i < chains.Length; i++) {
+			chains[i] = Instantiate(chainSection);
+			if (i == 0) {
+				chains[i].GetComponent<CharacterJoint>().connectedBody = rigidbody;
+				chains[i].transform.position = transform.position;
+				chains[i].GetComponent<CharacterJoint>().autoConfigureConnectedAnchor = false;
+				chains[0].GetComponent<CharacterJoint>().connectedAnchor = new Vector3(0, 0, 0);
+			} else {
+				chains[i].transform.SetParent(chains[i - 1].transform);
+				chains[i].transform.localPosition = offset;
+				chains[i].GetComponent<CharacterJoint>().connectedBody = chains[i - 1].GetComponent<Rigidbody>();
+			}
+
+			chains[i].SetActive(false);
+		}
+
+		chains[chains.Length - 1].AddComponent<Follow>();
+		chains[chains.Length - 1].GetComponent<Follow>().Setup(player.transform);
+
+		//gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(throwing) {
+		if (throwing) {
 			if (Vector3.Distance(end, transform.position) <= 1f || transform.position == end) {
 				throwing = false;
+				i = 0;
 			} else {
-				transform.position = Vector3.MoveTowards(transform.position, end, speed);
+				transform.position = Vector3.MoveTowards(transform.position, end, speed * Time.deltaTime);
 			}
 		} else {
-			if(done) {
-				gameObject.SetActive(false);
+			if (done) {
+				ResetChain();
+				//gameObject.SetActive(false);
 			} else {
-				cc.Move(Vector3.MoveTowards(player.transform.position, end, speed) - player.transform.position);
-				
+				cc.Move(Vector3.MoveTowards(player.transform.position, end, speed * Time.deltaTime) - player.transform.position);
+
 				//For the specific case that the main character class doesn't catch the collision. .3f is so it detects it after main character would.
-				if(Vector3.Distance(cc.ClosestPointOnBounds(end), end) <= .3f) {
+				if (Vector3.Distance(cc.ClosestPointOnBounds(end), end) <= .3f) {
 					done = true;
 				}
 			}
+		}
+	}
+
+	private void AddChain() {
+		for (int i = 0; i < chains.Length; i++) {
+			chains[i].SetActive(true);
+		}
+
+		chains[chains.Length - 1].GetComponent<Follow>().SetFollow(true);
+	}
+
+	private void ResetChain() {
+		for (int i = 0; i < chains.Length; i++) {
+			chains[i].SetActive(false);
 		}
 	}
 
@@ -46,14 +100,12 @@ public class Hook : MonoBehaviour {
 
 		transform.position = start;
 
-		if(cc == null) {
-			cc = player.GetComponent<CharacterController>();
-			gameObject.GetComponent<SphereCollider>().isTrigger = true;
-		}
-
 		gameObject.SetActive(true);
-		done = false;
 		throwing = true;
+		done = false;
+
+		AddChain();
+		
 	}
 
 	public bool Done() {
@@ -65,6 +117,7 @@ public class Hook : MonoBehaviour {
 	}
 
 	public void Stop() {
+		ResetChain();
 		done = true;
 	}
 
