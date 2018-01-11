0dff3582d9c1ddc090885049dbab536637ae7f3d
kgburdeshaw <kgburdeshaw@gmail.com>
2018-01-10 23:41:19 -0800

    First Commit
    Bare bines basics for grunt

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -10,6 +10,8 @@ public class Enemy : MonoBehaviour {
 	protected Health health;			//All enemies should have health or sometype of damage taking system
     protected NavMeshAgent agent;		//nav mesh agent on object 
     protected Transform target;			//the transform of the target to move towards
+    protected bool isAttacking;         //true if this enemy is in their attack animation
+    protected GameObject weapon;
 
 	//Enemy attributes
     protected float movementSpeed;		//speed of character
@@ -21,5 +23,28 @@ public class Enemy : MonoBehaviour {
 		agent = GetComponent<NavMeshAgent>();                   //set agent to this objects agent
 		target = GameObject.Find("Main Character").transform;    //set target to main character
 		tag = "Enemy";
-	}
+        isAttacking = false;
+    }
+
+    //if player is in trigger collider and not already attacking it will attack
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.transform == target && !isAttacking)
+        {
+            isAttacking = true;
+        }
+    }
+
+    //if collision occurs while attacking subtract health.
+    //*refine once animation is in place*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isAttacking)
+        {
+            //subtract player health
+            isAttacking = false;
+        }
+    }
+
+
 }
--- Assets/Scripts/Enemies/Grunt.cs
+++ Assets/Scripts/Enemies/Grunt.cs
@@ -4,10 +4,13 @@ using UnityEngine.AI;
 
 public class Grunt : Enemy {
 
+    Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		movementSpeed = 3.5f;
 		name = "Grunt";
+        aggroRange = 10;
+        rb = GetComponent<Rigidbody>();
 
 		agent.speed = movementSpeed;
 		health.SetHealth(10f);
@@ -15,11 +18,40 @@ public class Grunt : Enemy {
 	
 	// Update is called once per frame
 	void Update () {
-		agent.SetDestination(target.position);  //update agent destination to target location
 
+        //if enemy is in attack animation is won't move
+        if (isAttacking)
+        {
+            attack(); //temp "anim" for testing
+        }
+        else if (checkAgro())
+            agent.SetDestination(target.position);  //update agent destination to target location
+ 
 		//Check for death
 		if(health.GetHealth() <= 0) {
 			//Die
 		}
 	}
+
+    //true is in aggro range
+    private bool checkAgro()
+    {
+        return aggroRange > getDistToPlayer(); 
+    }
+
+    //finds the distance between character and grunt
+    private float getDistToPlayer()
+    {
+        return (-transform.position + target.position).magnitude;
+    }
+
+    //temporary attack "animation" for testing
+    private void attack()
+    {
+
+    }
+
+    
+
+
 }
