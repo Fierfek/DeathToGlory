a7ffd0142445867bb80087a0d21f198571668793
Jonathan Fong <robofin117@gmail.com>
2018-01-22 22:37:40 -0800

    Started on an Abomination Script and a Bomb script
    Abomination script not finished.
    Bomb script not finished.

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public float delay = 3f;
+    public float blastRadius = 5f;
+    public float explosionForce = 4f;
+
+    public GameObject explosionEffects;
+    float countdown;
+    bool hasExploded = false;
+	// Use this for initialization
+	void Start () {
+        countdown = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode(); 
+        }
+	}
+    
+    void Explode()
+    {
+        Instantiate(explosionEffects, transform.position, transform.rotation);
+
+        Collider [] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, blastRadius);
+            }
+        }
+
+        hasExploded = true;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemies/Abomination.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abomination : Enemy {
+
+    public float throwForce = 20f;
+    public float timer;
+    Animator anim;
+    public GameObject grenadePrefab;
+	// Use this for initialization
+	void Start () {
+        movementSpeed = 2.0f;
+        damageAmount = 3;
+        aggroRange = 8;
+        attackRange = 4f;
+        name = "Abomination";
+
+        anim = GetComponent<Animator>();
+        agent.speed = movementSpeed;
+        health.SetHealth(20f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ThrowBomb()
+    {
+        GameObject bomb = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = bomb.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
