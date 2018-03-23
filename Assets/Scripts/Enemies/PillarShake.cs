b3e5f3d5943293644e3d99645f19aa8c8dbb35bb
Jonathan Fong <robofin117@gmail.com>
2018-03-22 22:57:33 -0700

    Added Pillar Shake script and a pillar in EnemyTest scene.

---
+++ Assets/Scripts/Enemies/PillarShake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarShake : MonoBehaviour {
+
+    int range = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Shockwave")
+        {
+            ShakePillar();
+        }
+    }
+
+    void ShakePillar()
+    {
+        float xChange = transform.rotation.x + Random.Range(-5.0f, 5.0f);
+        float zChange = transform.rotation.z + Random.Range(-5.0f, 5.0f);
+        transform.Rotate(xChange, 0, zChange);
+        
+    }
+}
