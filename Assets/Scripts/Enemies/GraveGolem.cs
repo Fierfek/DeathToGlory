34c406aa87c4edf2d9d3cbffbf6fc89273cd1cfb
Jonathan Fong <robofin117@gmail.com>
2018-04-05 20:00:09 -0700

    Detecting closest pillar to Golem and throw player script done.
    Still needs work with holding the player for a period before throwing.
    Also, I need to test it.

--- Assets/Scripts/Enemies/GraveGolem.cs
+++ Assets/Scripts/Enemies/GraveGolem.cs
@@ -7,14 +7,16 @@ public class GraveGolem : Enemy {
     public GameObject gruntPrefab;
     public GameObject shockwave;
     public GameObject head;
-    public float cooldown = 15f;
-    float timer;
+    public float cooldown = 20f;
+    public float hurlRange = 10f;
+    public float hurlForce = 20f;
+    public float pillarDetectRnge = 200f; //This is the max pillar detect range for golem;
     public float spawnRange = 8f;
-
+    float timer;
 
     Animator anim;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         movementSpeed = 2.0f;
         damageAmount = 3;
         aggroRange = 8f;
@@ -26,10 +28,10 @@ public class GraveGolem : Enemy {
         health.SetHealth(20f);
         timer = cooldown;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if(health.GetHealth() <= 0)
+
+    // Update is called once per frame
+    void Update() {
+        if (health.GetHealth() <= 0)
         {
             isAttacking = false;
             anim.SetTrigger("Death");
@@ -37,15 +39,15 @@ public class GraveGolem : Enemy {
 
         else if (CheckAgro())
         {
-            if(cooldown> 0)
+            if (cooldown > 0)
             {
                 cooldown -= Time.deltaTime;
             }
-            if(getDistToPlayer() <= 5 && cooldown<=0)
+            if (getDistToPlayer() <= 2 && cooldown <= 0)
             {
-
+                ThrowPlayer();
             }
-            else if(getDistToPlayer() <= attackRange)
+            else if (getDistToPlayer() <= attackRange)
             {
                 Shockwave();
             }
@@ -65,13 +67,13 @@ public class GraveGolem : Enemy {
 
              */
         }
-	}
+    }
 
     void Shockwave()
     {
         GameObject poundAttack = Instantiate(shockwave);
 
-        
+
     }
 
     void SpawnEnemy()
@@ -88,7 +90,36 @@ public class GraveGolem : Enemy {
     //This script will throw player at a pillar.
     void ThrowPlayer()
     {
+        //Need to figure out how to hold the player for a time.
 
+        //Check if there is a pillar in range, then throw player to the nearest pillar.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, hurlRange);
+        Collider closestPillar = null;
+
+        float closeDist = pillarDetectRnge; //This is the max pillar detect range for golem;
+        foreach (Collider nearbyObject in colliders)
+        {
+            if (nearbyObject.tag == "Pillar")
+            {
+                float pillarDist = Vector3.Distance(nearbyObject.transform.position,transform.position);
+                if (closeDist > pillarDist)
+                {
+                    closeDist = pillarDist;
+                    closestPillar = nearbyObject;
+                }
+            }
+
+        }
+
+        //Golem turns towards closest pillar
+
+        //Then Throws player at pillar with some force.        
+        Rigidbody playerBody = target.GetComponent<Rigidbody>();
+        playerBody.AddRelativeForce(closestPillar.transform.position * hurlForce, ForceMode.VelocityChange);
+
+        //Damage player due to impact or grab.
     }
+
+
 
 }
