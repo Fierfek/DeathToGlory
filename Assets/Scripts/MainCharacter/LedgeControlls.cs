b129d4882afa9788494806d0420d356f06ffe94a
Fierfek <geekssw@yahoo.com>
2018-04-12 19:14:35 -0700

    Added Attack Animations

--- Assets/Scripts/MainCharacter/LedgeControlls.cs
+++ Assets/Scripts/MainCharacter/LedgeControlls.cs
@@ -47,7 +47,6 @@ public class LedgeControlls : MonoBehaviour {
 			firstTime = false;
 		}
 
-		
 	}
 
 	// Update is called once per frame
--- Assets/Scripts/MainCharacter/MainCharacter.cs
+++ Assets/Scripts/MainCharacter/MainCharacter.cs
@@ -12,6 +12,7 @@ public class MainCharacter : MonoBehaviour {
 	CharacterController cc;
 	LedgeControlls lc;
 	MouseRotation mr;
+	Animator anim;
 			
 	public static bool updateStats;
 	public Shotgun shotgun;
@@ -36,6 +37,7 @@ public class MainCharacter : MonoBehaviour {
 		cm = GetComponent<CharacterMovement>();
 		cc = GetComponent<CharacterController>();
 		lc = GetComponent<LedgeControlls>();
+		anim = GetComponentInChildren<Animator>();
 		lc.enabled = false;
 		mr = GetComponentInChildren<MouseRotation>();
 
@@ -116,7 +118,7 @@ public class MainCharacter : MonoBehaviour {
 	}
 
 	public void BasicAttack() {
-
+		anim.SetTrigger("Attack");
 	}
 
 	//This checks if you collide with something before getting to the point you hooked;
